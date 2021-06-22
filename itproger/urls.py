879f192591c1634ca22470a5f5fb1f906d6e9f13
Aleksandr <aleksandrshafikov15@gmail.com>
2021-06-22 21:16:23 +0300

    Account settings, adding a photo

--- itproger/urls.py
+++ itproger/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as userViews
-from  django.contrib.auth import views as authViews
+from django.contrib.auth import views as authViews
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,5 @@ urlpatterns = [
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
     path('', include("blog.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
